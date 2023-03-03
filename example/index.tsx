a55820fcbc45fedf9cce654f5b895dc057008a68
Nareg tokatlian <naregtokatlian@gmail.com>
2023-03-03 03:21:38 -0500

    added example and fixed parcel bug

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,23 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AiText } from 'react-text-aianimation';
 
 const App = () => {
   return (
-    <div>
-      <Thing />
+    <div style={{ background: 'black', height: '100vh' }}>
+      <div style={{ minWidth: '400px' }}>
+        <AiText
+          text="terminator"
+          speed={100}
+          styles={{
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: '5rem',
+          }}
+          loops={3}
+        />
+      </div>
     </div>
   );
 };
